0a35ff473fbe9254809aedbe802dfb33d5b1cd2e
David Carr <d.carr@corecomtechacademy.co.uk>
2026-04-15 14:09:48 +0100

    Add forEach method to interate over cells

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
 const startBtn = document.getElementById("start-game-btn");
-const cell = document.querySelector('.cell:nth-child(1)');
-const cellTwo = document.querySelector('.cell:nth-child(2)');
-const cellThree = document.querySelector('.cell:nth-child(3)');
-const cellFour = document.querySelector('.cell:nth-child(4)');
-const cellFive = document.querySelector('.cell:nth-child(5)');
-const cellSix = document.querySelector('.cell:nth-child(6)');
-const cellSeven = document.querySelector('.cell:nth-child(7)');
-const cellEight = document.querySelector('.cell:nth-child(8)');
-const cellNine = document.querySelector('.cell:nth-child(9)');
+const cell = document.getElementsByClassName('cell');
 
 startBtn.addEventListener("click", () => {
     startBtn.remove();
@@ -16,34 +8,19 @@ startBtn.addEventListener("click", () => {
     })
 });
 
-cell.addEventListener('click', () => {
-    cell.style.background = "green";
-    });
-cellOne.addEventListener('click', () => {
-    cell1.style.background = "blue";
-    });
-    cell.addEventListener('click', () => {
-    cell.style.background = "green";
-    });
-cellTwo.addEventListener('click', () => {
-    cell1.style.background = "blue";
-    });cell.addEventListener('click', () => {
-    cell.style.background = "green";
-    });
-cellThree.addEventListener('click', () => {
-    cell1.style.background = "blue";
-    });cell.addEventListener('click', () => {
-    cell.style.background = "green";
-    });
-cellFour.addEventListener('click', () => {
-    cell1.style.background = "blue";
-    });
+const cells = Array.from(document.getElementsByClassName("cell"))
+cells.forEach((cell, index) => {
+cell.addEventListener("click", () => {
+const img = document.createElement("img")
+img.src = "../assets/images/cross.png"
+img.alt = "X"
+cell.computedStyleMap.backgroundSize = "contain";
+cell.computedStyleMap.backgroundRepeat = "no-repeat";
+cell.computedStyleMap.backgroundPosition = "center";
+cell.appendChild(img);
+})
+})
 
-// const cells = document.querySelectorAll(".grid.cell");
-// cells.forEach((cell, index) => {
-//     cell.addEventListener("click", () =>
-//         console.log(index);
-// })
 
 // const crossClick = document.
 // const noughtClick
