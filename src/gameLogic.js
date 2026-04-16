24c71605acd28eb3a559d111a722b6d6344fa070
David Carr <d.carr@corecomtechacademy.co.uk>
2026-04-16 19:00:03 +0100

    Add alert when user clicks on same image

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,25 +1,31 @@
 const cell = document.getElementsByClassName('cell');
 const cells = Array.from(document.getElementsByClassName("cell"))
+
 const images = {
     X: "../assets/images/cross.png",
     O: "../assets/images/nought.png"
 }
+
 let currentPlayer = "X"
 
 const getNextPlayer = () => {
     currentPlayer = currentPlayer === "X" ? "O" : "X"
     console.log("Current player is now:", currentPlayer)
-}
+    }
 
 cells.forEach((cell, index) => {
-cell.addEventListener("click", () => {
+    cell.addEventListener("click", () => {
+    if (cell.querySelector("img")) {
+    alert("You cannot click on the same square!")
+    return
+    }
 const img = document.createElement("img")
 img.src = images[currentPlayer]
 img.alt = currentPlayer
-getNextPlayer()
-cell.computedStyleMap.backgroundSize = "contain";
-cell.computedStyleMap.backgroundRepeat = "no-repeat";
-cell.computedStyleMap.backgroundPosition = "center";
+cell.style.backgroundSize = "contain";
+cell.style.backgroundRepeat = "no-repeat";
+cell.style.backgroundPosition = "center";
 cell.appendChild(img);
+getNextPlayer()
 });
 });
