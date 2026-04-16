3800e52778584705d596a42255193e6dcc8c95be
David Carr <d.carr@corecomtechacademy.co.uk>
2026-04-16 18:44:33 +0100

    Add switch player logic with images

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,21 +1,25 @@
 const cell = document.getElementsByClassName('cell');
 const cells = Array.from(document.getElementsByClassName("cell"))
+const images = {
+    X: "../assets/images/cross.png",
+    O: "../assets/images/nought.png"
+}
 let currentPlayer = "X"
+
+const getNextPlayer = () => {
+    currentPlayer = currentPlayer === "X" ? "O" : "X"
+    console.log("Current player is now:", currentPlayer)
+}
 
 cells.forEach((cell, index) => {
 cell.addEventListener("click", () => {
 const img = document.createElement("img")
-img.src = "../assets/images/cross.png"
-img.alt = "X"
+img.src = images[currentPlayer]
+img.alt = currentPlayer
+getNextPlayer()
 cell.computedStyleMap.backgroundSize = "contain";
 cell.computedStyleMap.backgroundRepeat = "no-repeat";
 cell.computedStyleMap.backgroundPosition = "center";
 cell.appendChild(img);
 });
 });
-
-if (currentPlayer === "X") {
-    cell.innerHTML = "../assets/images/cross.png"
-} else {
-    cell.innerHTML = "../assets/images/nought.png"
-}
